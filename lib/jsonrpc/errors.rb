6ad27b2f2fc130eaa7e8990ddc41aa7cc7c58669
Néstor Salceda <nestor.salceda@gmail.com>
2017-04-27 16:48:43 +0200

    Implement all error codes in json rpc 2

--- lib/jsonrpc/errors.rb
+++ lib/jsonrpc/errors.rb
@@ -1,19 +1,40 @@
 module JsonRPC
-  class InvalidJSONError < StandardError
+  class Error < StandardError
     attr_reader :code
 
-    def initialize
-      @code = -32700
-      super("Parse error")
+    def initialize(code, message)
+      @code = code
+      super(message)
     end
   end
 
-  class InvalidRequestError < StandardError
-    attr_reader :code
-
+  class InvalidJSONError < Error
     def initialize
-      @code = -32600
-      super("Invalid Request")
+      super(-32700, "Parse error")
+    end
+  end
+
+  class InvalidRequestError < Error
+    def initialize
+      super(-32600, "Invalid Request")
+    end
+  end
+
+  class MethodNotFoundError < Error
+    def initialize
+      super(-32601, "Method not found")
+    end
+  end
+
+  class InvalidParamsError < Error
+    def initialize
+      super(-32602, "Invalid params")
+    end
+  end
+
+  class InternalError < Error
+    def initialize
+      super(-32603, "Internal error")
     end
   end
 end
